c23a1cdaeaeda4aca0f21d423d17f4b6abb6c311
windpo <2932213149@qq.com>
2022-03-14 15:13:46 +0800

    完成了正则表达式的特别分组

---
+++ src/DateTest.java
@@ -0,0 +1,11 @@
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class DateTest {
+    public static void main(String[] args) {
+        Date today=new Date();//初始化时间（为当前默认时间）
+        SimpleDateFormat ft=new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");//创造自定义格式化时期的对象实例
+        System.out.println(today.toString());//tostring方法打印时间
+        System.out.println("当前时间为："+ft.format(today));
+    }
+}
